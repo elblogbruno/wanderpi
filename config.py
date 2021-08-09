2a721b52fe8995c1113102136755d480835b2904
elblogbruno <elblogdebruno@outlook.es>
2021-08-09 19:58:10 +0200

    fix issues with comments

--- config.py
+++ config.py
@@ -8,9 +8,8 @@ VIDEOS_FOLDER = os.getcwd()+'/controller/static/wanderpis/'
 
 
 class Config:
-    # 调试模式
+    
     DEBUG = True
-    # session加密秘钥
     SECRET_KEY = "fM3PEZwSRcbLkk2Ew82yZFffdAYsNgOddWoANdQo/U3VLZ/qNsOKLsQPYXDPon2t"
 
     MAX_CONTENT_LENGTH  = 200 * 1024 * 1024
@@ -21,24 +20,19 @@ class Config:
     DROPZONE_PARALLEL_UPLOADS=3  # set parallel amount
     DROPZONE_UPLOAD_MULTIPLE=True  # enable upload multiple
 
-    # session过期时间
     PERMANENT_SESSION_LIFETIME = timedelta(days=7)
 
 
 class DevelopmentConfig(Config):
-    # 开发模式配置
     DEBUG = True
     LOG_LEVEL = logging.DEBUG
     UPLOAD_FOLDER = UPLOAD_FOLDER
 
 class ProductionConfig(Config):
-    # 上线配置
-    # 关闭调试
     DEBUG = False
-    LOG_LEVEL = logging.ERROR  # 日志级别
+    LOG_LEVEL = logging.ERROR  
 
 
-# 配置字典，键：配置
 config_dict = {
     'dev': DevelopmentConfig,
     'pro': ProductionConfig
