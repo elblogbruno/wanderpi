ed28841f385e169748b7c75ec64cf268d46c7abb
elblogbruno <elblogdebruno@outlook.es>
2021-08-22 18:30:07 +0100

    fixed download button

--- controller/modules/files_utils/views.py
+++ controller/modules/files_utils/views.py
@@ -7,6 +7,36 @@ from controller.models.models import Wanderpi
 from controller.modules.files_utils import files_utils_blu
 from controller.utils.video_editor import VideoEditor
 from controller.utils.image_editor import ImageEditor
+from config import load_custom_video_folder
+import os
+from uuid import UUID
+
+def is_valid_uuid(uuid_to_test, version=4):
+    """
+    Check if uuid_to_test is a valid UUID.
+    
+     Parameters
+    ----------
+    uuid_to_test : str
+    version : {1, 2, 3, 4}
+    
+     Returns
+    -------
+    `True` if uuid_to_test is a valid UUID, otherwise `False`.
+    
+     Examples
+    --------
+    >>> is_valid_uuid('c9bf9e57-1685-4c89-bafb-ff5af830be8a')
+    True
+    >>> is_valid_uuid('c9bf9e58')
+    False
+    """
+    
+    try:
+        uuid_obj = UUID(uuid_to_test, version=version)
+    except ValueError:
+        return False
+    return str(uuid_obj) == uuid_to_test
 
 @files_utils_blu.route('/download_file/<string:travel_id>/<string:filename>', methods=['GET'])
 def download_file(travel_id, filename):
@@ -14,8 +44,12 @@ def download_file(travel_id, filename):
     file = Wanderpi.get_by_id(filename)
     file_type = 'images' if file.is_image else 'videos'
     abs_file_path = get_travel_folder_path(travel_id=travel_id, file_type=file_type)
-    #file_path = get_travel_folder_path(travel_id=travel_id, filename_or_file_id=filename, file_type=file_type)
+    file_path = get_travel_folder_path(travel_id=travel_id, filename_or_file_id=filename, file_type=file_type)
     
+    # check if file exists, if it does not, maybe it is a custom video or photo
+    if not os.path.exists(file_path):
+        filename = Wanderpi.get_by_id(filename).name
+
     if '.' not in filename: 
         filename = file.file_path.split('/')[-1]
     
